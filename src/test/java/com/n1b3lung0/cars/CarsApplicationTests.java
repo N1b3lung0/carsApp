e6ec3a2859ef67f873a4ee6ed96f3cfc68256e62
n1b3lung0 <carlos.martinez.crespo@gmail.com>
2020-12-07 20:39:38 +0100

    first commit

---
+++ src/test/java/com/n1b3lung0/cars/CarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n1b3lung0.cars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
